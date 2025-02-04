6148497d68b92898b3e1064f89f3d4a581c72349
ball1024 <kisekung6@gmail.com>
2025-02-04 20:35:38 +0700

    First Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
 // JS here
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('show')
+    
+})
+
+let topBtn = document.getElementById("btt");
+
+function scollFunc() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topBtn.style.display = "block";
+    } else{
+        topBtn.style.display = "none";
+    }
+}
+
+window.onscroll = function() {
+    scollFunc();
+}
+
+function backToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
+}
